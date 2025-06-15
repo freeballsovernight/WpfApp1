5466c8310015321a91244d4493df45774c67a78d
Adam Jafarov <thevakhovske@petalmail.com>
2025-06-15 11:29:52 +0300

    Add project files.

---
+++ WpfApp1/Model/Car.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Model
+{
+    public class Car : INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+        private string manufacturerString; //имя сотрудника
+        public string Manufacturer
+        {
+            get { return manufacturerString; }
+            set
+            {
+                manufacturerString = value;
+                OnPropertyChanged("Manufacturer");
+            }
+        }
+        private string modelString; //фамилия сотрудника
+        public string Model
+        {
+            get { return modelString; }
+            set
+            {
+                modelString = value;
+                OnPropertyChanged("Model");
+            }
+        }
+        private int quantity; // должность сотрудника
+        public int Quantity
+        {
+            get { return quantity; }
+            set
+            {
+                quantity = value;
+                OnPropertyChanged("Quantity");
+            }
+        }
+        private DateTime arrivalDate; //дата рождения сотрудника
+        public DateTime Arrival
+        {
+            get { return arrivalDate; }
+            set
+            {
+                arrivalDate = value;
+                OnPropertyChanged("Arrival");
+            }
+        }
+        public Car() { }
+        public Car(int id, string manufacturerString, string modelString, int quantity, DateTime
+       arrivalDate)
+        {
+            this.Id = id;
+            this.Manufacturer = manufacturerString;
+            this.Model = modelString;
+            this.Quantity = quantity;
+            this.Arrival = arrivalDate;
+        }
+        //Реализация интерфейса INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ WpfApp1/ViewModel/CarViewModel.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfApp1.Helper;
+using WpfApp1.Model;
+using WpfApp1.View;
+
+
+namespace WpfApp1.ViewModel
+{
+    public class CarViewModel : INotifyPropertyChanged
+    {
+        // Нахождение максимального Id в коллекции данных
+        public int MaxId()
+        {
+            int max = 0;
+            foreach (var per in this.ListCar)
+            {
+                if (max < per.Id)
+                {
+                    max = per.Id;
+                };
+            }
+            return max;
+        }
+        // добавление сотрудника
+        private RelayCommand addCar;
+        public RelayCommand AddCar
+        {
+            get
+            {
+                return addCar ??
+                (addCar = new RelayCommand(obj =>
+                {
+                    WindowNewCar wnCar = new WindowNewCar
+                    {
+                        Title = "Новый сотрудник"
+                    };
+                    // формирование кода нового сотрудника
+                    int maxIdCar = MaxId() + 1;
+                    Car person = new Car
+                    {
+                        Id = maxIdCar,
+                        Arrival = DateTime.Now
+                    };
+                    wnCar.DataContext = person;
+                    if (wnCar.ShowDialog() == true)
+                    {
+                        ListCar.Add(person);
+                    }
+                    SelectedCar = person;
+                }));
+            }
+        }
+        // команда редактирования данных по сотруднику
+        private RelayCommand editCar;
+        public RelayCommand EditCar
+        {
+            get
+            {
+                return editCar ??
+                (editCar = new RelayCommand(obj =>
+                {
+                    WindowNewCar wnCar = new WindowNewCar()
+                    {
+                        Title = "Редактирование данных сотрудника",
+                    };
+                    Car person = SelectedCar;
+                    Car tempCar = person;
+                    wnCar.DataContext = tempCar;
+                    if (wnCar.ShowDialog() == true)
+                    {
+                        // сохранение данных в оперативной памяти
+                        person = tempCar;
+                    }
+                }, (obj) => SelectedCar != null && ListCar.Count > 0));
+            }
+        }
+        // Удаление данных по сотруднику
+        private RelayCommand deleteCar;
+        public RelayCommand DeleteCar
+        {
+            get
+            {
+                return deleteCar ??
+                (deleteCar = new RelayCommand(obj =>
+                {
+                    Car person = SelectedCar;
+                    MessageBoxResult result = MessageBox.Show("Удалить данные по сотруднику: \n" +
+    person.Model + " " + person.Manufacturer,
+    "Предупреждение", MessageBoxButton.OKCancel, MessageBoxImage.Warning);
+                    if (result == MessageBoxResult.OK)
+                    {
+                        // удаление данных в списке отображения данных
+                        ListCar.Remove(person);
+                    }
+                }, (obj) => SelectedCar != null && ListCar.Count > 0));
+            }
+        }
+
+        private Car selectedCar; //выделенные в списке данные по сотруднику
+        public Car SelectedCar
+        {
+            get { return selectedCar; }
+            set
+            {
+                selectedCar = value;
+                OnPropertyChanged("SelectedCar");
+            }
+        }
+        // коллекция данных по сотрудникам
+        public ObservableCollection<Car> ListCar { get; set; } =
+        new ObservableCollection<Car>();
+        public CarViewModel()
+        {
+            this.ListCar.Add(new Car
+            {
+                Id = 1,
+                Manufacturer = "Lixiang",
+                Model = "L9",
+                Quantity = 6,
+                Arrival = new DateTime(2024, 02, 28)
+            });
+            this.ListCar.Add(new Car
+            {
+                Id = 2,
+                Manufacturer = "BMW",
+                Model = "M3 GTR",
+                Quantity = 1,
+                Arrival = new DateTime(2024, 03, 20)
+            });
+            this.ListCar.Add(new Car
+            {
+                Id = 3,
+                Manufacturer = "Changan",
+                Model = "UNI-V",
+                Quantity = 4,
+                Arrival = new DateTime(2024, 04, 15)
+            });
+            this.ListCar.Add(new Car
+            {
+                Id = 4,
+                Manufacturer = "Xiaomi",
+                Model = "SU7",
+                Quantity = 2,
+                Arrival = new DateTime(2025, 05, 10)
+            });
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName]
+string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
